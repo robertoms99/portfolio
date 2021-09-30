bac4616ae2cc0f46adbcb97181ad4329fd1c932e
Roberto Molina Silvera <rmolinas1999@gmail.com>
2021-09-30 13:56:43 -0500

    feat: implementacion enlace directo a proyectos

--- src/views/components/sections/AboutSection/components/Banner/Banner.tsx
+++ src/views/components/sections/AboutSection/components/Banner/Banner.tsx
@@ -1,12 +1,14 @@
 import cn from 'classnames'
-import {Button} from '../../../../ui'
+import {Button, Link} from '../../../../ui'
 
 const Banner = () =>{
     return (
         <div className="banner">
             <h4 className="[ heading heading--accent ] [ profession ] [ caret ]">software developer</h4>
             <h1 className="heading heading--primary name">Roberto Molina/&gt;</h1>
-            <Button.Primary className="cta">view my projects</Button.Primary>
+            <Link href="#projects">
+                <Button.Primary className="cta">view my projects</Button.Primary>
+            </Link>
         </div>
     )
 }
