3c723b927614b95e9c48104aff3dd4a6d14e37d3
Roberto Molina Silvera <rmolinas1999@gmail.com>
2021-10-25 14:10:44 -0500

    fix: correccion useCaretify

--- src/views/hooks/useCaretify/useCaretify.ts
+++ src/views/hooks/useCaretify/useCaretify.ts
@@ -1,74 +1,51 @@
-import { useCallback, useEffect, useMemo } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { throttle } from '../../../lib/wrapperify'
-import useForceUpdate from '../useForceUpdate'
+
+const DOM_STRINGS = {
+  caret: 'caret',
+  caretStarting: 'caret-animation-starting',
+  caretFinished: 'caret-animation-finished'
+}
+
+const CARETIFY_TIMEOUTS = {
+  throttling: 150,
+  typing: 70
+}
 
 const useCaretify = () => {
-  const DOMStrings = useMemo(
-    () => ({
-      caret: 'caret',
-      caretStarting: 'caret-animation-starting',
-      caretFinished: 'caret-animation-finished'
-    }),
-    []
-  )
+  const [caretElements, setCaretElements] = useState<HTMLCollectionOf<Element> | null>(null)
 
-  const forceUpdate = useForceUpdate()
-
-  const caretElements = document.getElementsByClassName(DOMStrings.caret) ?? []
-
-  const caretifyTimeouts = useMemo(
-    () => ({
-      throttling: 150,
-      typing: 70
-    }),
-    []
-  )
-
-  const caretElementsContentMap = (() => {
-    const contentElementsMap = new Map<Element, string>()
-    for (let index = 0; index < caretElements.length; index++) {
-      const caretElement = caretElements[index]
-      contentElementsMap.set(caretElement, caretElement.textContent ?? '')
-      caretElement.innerHTML = '&nbsp;'
-    }
-    return contentElementsMap
-  })()
-
-  const typeCaretAnimation = useCallback(
-    (caretElement: Element) => {
-      caretElement.classList.toggle(DOMStrings.caretStarting, true)
-      const caretElementContent = caretElementsContentMap.get(caretElement) ?? ''
-      let index = 0
-      const intervalId = setInterval(() => {
-        caretElement.textContent = `${caretElement.textContent ?? ''}${caretElementContent[index]}`
-        index++
-        if (index === caretElementContent.length) clearCaretAnimation(caretElement, intervalId)
-      }, caretifyTimeouts.typing) as unknown as number
-    },
-    [caretElementsContentMap]
+  const [caretElementsContentMap, setCaretElementsContentMap] = useState<Map<Element, string>>(
+    new Map()
   )
 
   const clearCaretAnimation = useCallback((caretElement: Element, intervalId: number) => {
     clearInterval(intervalId)
     setTimeout(() => {
-      caretElement.classList.toggle(DOMStrings.caretStarting, false)
-      caretElement.classList.toggle(DOMStrings.caretFinished, true)
+      caretElement.classList.toggle(DOM_STRINGS.caretStarting, false)
+      caretElement.classList.toggle(DOM_STRINGS.caretFinished, true)
     }, 1000)
   }, [])
 
-  const caretElementIsVisible = useCallback((caretElement: Element) => {
-    const { top } = caretElement.getBoundingClientRect()
-    const elementPageOffsetY = top + window.pageYOffset
-    const pageOffsetBottom = window.pageYOffset + window.innerHeight
-    return elementPageOffsetY < pageOffsetBottom && elementPageOffsetY > window.pageYOffset
-  }, [])
+  const typeCaretAnimation = (caretElement: Element) => {
+    caretElement.classList.toggle(DOM_STRINGS.caretStarting, true)
+    const caretElementContent = caretElementsContentMap.get(caretElement) ?? ''
+    let index = 0
+    if (caretElementContent.length === 0) return
+    const intervalId = setInterval(() => {
+      caretElement.textContent = `${caretElement.textContent ?? ''}${caretElementContent[index]}`
+      index++
+      if (index === caretElementContent.length) clearCaretAnimation(caretElement, intervalId)
+    }, CARETIFY_TIMEOUTS.typing) as unknown as number
+  }
 
   const caretElementWasAnimated = useCallback((caretElement: Element) => {
-    return caretElement.matches(`${DOMStrings.caretStarting},${DOMStrings.caretFinished}`)
+    return caretElement.matches(`.${DOM_STRINGS.caretStarting},.${DOM_STRINGS.caretFinished}`)
   }, [])
 
-  const attachCaretScrollEvent = () => {
-    const handleScrollSectionThrottled = throttle(() => {
+  const handleScrollSectionThrottled = useCallback(
+    throttle(() => {
+      if (caretElements === null || caretElementsContentMap.size === 0) return
       window.requestAnimationFrame(() => {
         for (let index = 0; index < caretElements.length; index++) {
           const caretElement = caretElements[index]
@@ -77,22 +54,48 @@ const useCaretify = () => {
           }
         }
       })
-    }, caretifyTimeouts.throttling)
+    }, CARETIFY_TIMEOUTS.throttling),
+    [caretElements, caretElementsContentMap]
+  )
 
-    window.onscroll = handleScrollSectionThrottled
+  const caretElementIsVisible = useCallback((caretElement: Element) => {
+    const { top } = caretElement.getBoundingClientRect()
+    const elementPageOffsetY = top + window.pageYOffset
+    const pageOffsetBottom = window.pageYOffset + window.innerHeight
+    return elementPageOffsetY < pageOffsetBottom && elementPageOffsetY > window.pageYOffset
+  }, [])
+
+  const componentDidMount = () => {
+    setCaretElements(document.getElementsByClassName(DOM_STRINGS.caret))
+  }
+
+  const componentDidUpdate = () => {
+    if (caretElements === null) return
+    const contentElementsMap = new Map<Element, string>()
+    for (let index = 0; index < caretElements.length; index++) {
+      const caretElement = caretElements[index]
+      contentElementsMap.set(caretElement, caretElement.textContent ?? '')
+      caretElement.innerHTML = '&nbsp;'
+    }
+
+    setCaretElementsContentMap(contentElementsMap)
+  }
+
+  useEffect(componentDidMount, [])
+
+  useEffect(componentDidUpdate, [caretElements])
+
+  useEffect(() => {
+    if (caretElementsContentMap.size === 0) return
+
     window.onload = handleScrollSectionThrottled
+    window.onscroll = handleScrollSectionThrottled
 
     return () => {
       window.onscroll = null
       window.onload = null
     }
-  }
-
-  useEffect(attachCaretScrollEvent, [caretElements])
-
-  useEffect(() => {
-    forceUpdate()
-  }, [])
+  }, [caretElementsContentMap])
 }
 
 export default useCaretify
